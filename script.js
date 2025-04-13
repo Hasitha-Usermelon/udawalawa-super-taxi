fdeb01f9d4991341dc4e843bf657b942dd730da6
Lakshan H M H <157282954+Hasitha-Usermelon@users.noreply.github.com>
2025-04-13 21:13:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-image');
+
+function showImage(index) {
+  images.forEach((img, i) => {
+    img.style.display = i === index ? 'block' : 'none';
+  });
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+showImage(currentIndex);
+setInterval(nextImage, 4000); 
+
